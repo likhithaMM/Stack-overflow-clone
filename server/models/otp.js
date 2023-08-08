b9dc93832bdfd0873f0544567be18e66cd7239b6
Medisetty Likhitha <likhithamedisetty22@gmail.com>
2023-08-08 23:05:46 +0530

    seven commits

--- server/models/otp.js
+++ server/models/otp.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const OTPSchema = mongoose.Schema({
     email: { type: String, required: true },
     otp: { type: String, required: true },
-    expiresIn: { type: Date, required:true }
+   
 })
 
 export default mongoose.model('OTP', OTPSchema) 
