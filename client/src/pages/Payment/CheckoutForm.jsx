09eb348c2454e671ebb587e18800582f520c976c
Medisetty Likhitha <likhithamedisetty22@gmail.com>
2023-08-10 15:11:50 +0530

    l commits

--- client/src/pages/Payment/CheckoutForm.jsx
+++ client/src/pages/Payment/CheckoutForm.jsx
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/Success",
+        return_url: process.env.REACT_APP_FRONT_END,
       },
     });
 
